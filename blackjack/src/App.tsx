c3459fbb3bea58560fec4c9802be0adb31581216
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-01-16 00:42:55 -0500

    feat(PlayingCard): Basic playing card suits implementation

--- blackjack/src/App.tsx
+++ blackjack/src/App.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import { Grid } from "@mui/system";
-import PlayingCard from "./shared/components/PlayingCard.tsx";
+import PlayingCard, { card_values } from "./shared/components/PlayingCard.tsx";
 
 function App() {
 	return (
 		<Grid container>
-			<Grid size={12}>
-				<PlayingCard />
+			<Grid container size={12}>
+				{card_values.map((char) => (
+					<PlayingCard character={char} />
+				))}
+			</Grid>
+			<Grid container size={12}>
+				{card_values.map((char) => (
+					<PlayingCard variant="clubs" character={char} />
+				))}
+			</Grid>
+			<Grid container size={12}>
+				{card_values.map((char) => (
+					<PlayingCard variant="diamonds" character={char} />
+				))}
+			</Grid>
+			<Grid container size={12}>
+				{card_values.map((char) => (
+					<PlayingCard variant="spades" character={char} />
+				))}
 			</Grid>
 		</Grid>
 	);
--- blackjack/src/shared/components/PlayingCard.tsx
+++ blackjack/src/shared/components/PlayingCard.tsx
@@ -1,19 +1,51 @@
 import React from "react";
 import "./PlayingCard.css";
 
-function PlayingCard() {
+const styles = {
+	hearts: { icon: "\u2665", color: "red" },
+	diamonds: { icon: "\u2666", color: "red" },
+	clubs: { icon: "\u2663", color: "black" },
+	spades: { icon: "\u2660", color: "black" },
+};
+
+export const card_values = [
+	"A",
+	"K",
+	"Q",
+	"J",
+	"10",
+	"9",
+	"8",
+	"7",
+	"6",
+	"5",
+	"4",
+	"3",
+	"2",
+];
+
+function PlayingCard({ variant = "hearts", character = "A" }) {
+	const cardStyle = styles[variant] || styles["hearts"];
+
 	return (
-		<div className="playing-card">
+		<div className="playing-card" style={{ color: cardStyle.color }}>
 			<div className="playing-card-top">
-				<span className="playing-card-text">A</span>
-				<span className="playing-card-symbol-edge">&#9829;</span>
+				<span className="playing-card-text">{character}</span>
+				<span className="playing-card-symbol-edge">
+					{cardStyle.icon}
+				</span>
 			</div>
 			<div className="playing-card-center">
-				<div className="playing-card-symbol">&#9829;</div>
+				<div className="playing-card-symbol">{cardStyle.icon}</div>
 			</div>
 			<div className="playing-card-bottom">
-				<span className="playing-card-text">A</span>
-				<span className="playing-card-symbol-edge">&#9829;</span>
+				<span className="playing-card-text">{character}</span>
+				<span
+					className="playing-card-symbol-edge"
+					color={cardStyle.color}
+				>
+					{cardStyle.icon}
+				</span>
 			</div>
 		</div>
 	);
