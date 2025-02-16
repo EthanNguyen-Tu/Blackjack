a7f8c1328cda9bd025fc5ab33c5b244a46c682e0
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-02-16 17:01:39 -0500

    refactor(components): organized each component's files into its own folder

--- blackjack/src/App.tsx
+++ blackjack/src/App.tsx
@@ -1,48 +1,47 @@
 import React, { useState } from "react";
 import { Grid } from "@mui/system";
-import Settings from "./shared/components/Settings.tsx";
+import Settings from "./shared/components/Settings/Settings.tsx";
 import AllCards from "./pages/AllCards.tsx";
 import { AppBar, Toolbar, Typography } from "@mui/material";
-import Table from "./shared/components/Table.tsx";
+import Table from "./shared/components/Table/Table.tsx";
 
 function App() {
-	const [showAllCards, setShowAllCards] = useState(true);
+    const [showAllCards, setShowAllCards] = useState(true);
 
-	const toggleAllCards = () => setShowAllCards(!showAllCards);
+    const toggleAllCards = () => setShowAllCards(!showAllCards);
 
-	return (
-		<Grid container>
-			<Grid size={12}>
-				<AppBar position="static" sx={{ backgroundColor: "#c9bfab" }}>
-					<Toolbar>
-						<Typography
-							marginLeft="5%"
-							fontSize={"3rem"}
-							sx={{
-								flexGrow: 1,
-								color: "primary.main",
-								fontWeight: "bold",
-							}}
-						>
-							Blackjack
-						</Typography>
-						<Settings
-							showAllCards={showAllCards}
-							toggleAllCards={toggleAllCards}
-						/>
-					</Toolbar>
-				</AppBar>
-			</Grid>
-			<Grid container size={12} justifyContent={"center"}>
-				<Table
-					dealer_hand={["Ah", "Ac", "Qh", "Qc", "Jd", "Js"]}
-					player_hand={["Ad", "As", "Kh", "Kc", "10d", "10s"]}
-				/>
-			</Grid>
-			{showAllCards && <AllCards />}
-		</Grid>
-	);
+    return (
+        <Grid container>
+            <Grid size={12}>
+                <AppBar position="static" sx={{ backgroundColor: "#c9bfab" }}>
+                    <Toolbar>
+                        <Typography
+                            marginLeft="5%"
+                            fontSize={"3rem"}
+                            sx={{
+                                flexGrow: 1,
+                                color: "primary.main",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            Blackjack
+                        </Typography>
+                        <Settings
+                            showAllCards={showAllCards}
+                            toggleAllCards={toggleAllCards}
+                        />
+                    </Toolbar>
+                </AppBar>
+            </Grid>
+            <Grid container size={12} justifyContent={"center"}>
+                <Table
+                    dealer_hand={["Ah", "Ac", "Qh", "Qc", "Jd", "Js"]}
+                    player_hand={["Ad", "As", "Kh", "Kc", "10d", "10s"]}
+                />
+            </Grid>
+            {showAllCards && <AllCards />}
+        </Grid>
+    );
 }
 
 export default App;
-
--- blackjack/src/pages/AllCards.tsx
+++ blackjack/src/pages/AllCards.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Grid } from "@mui/system";
-import PlayingCard, { cards } from "./../shared/components/PlayingCard.tsx";
+import PlayingCard, {
+    cards,
+} from "../shared/components/PlayingCard/PlayingCard.tsx";
 
 function AllCards() {
-	return (
-		<Grid container>
-			<Grid container size={12}>
-				{cards.map((card) => (
-					<PlayingCard card={card} />
-				))}
-			</Grid>
-		</Grid>
-	);
+    return (
+        <Grid container>
+            <Grid container size={12}>
+                {cards.map((card) => (
+                    <PlayingCard card={card} />
+                ))}
+            </Grid>
+        </Grid>
+    );
 }
 
 export default AllCards;
