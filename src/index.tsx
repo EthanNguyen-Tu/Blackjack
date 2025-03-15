6b1ccb5016f03baa98b3f83e5284b1ce53f83654
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-03-15 10:29:33 -0400

    style(Settings): Modified to better use Material UI capabilities

--- src/index.tsx
+++ src/index.tsx
@@ -6,64 +6,81 @@ import reportWebVitals from "./reportWebVitals.ts";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
-	palette: {
-		mode: "light",
-		primary: {
-			main: "#6d442b",
-			light: "#b97b58",
-		},
-		secondary: {
-			main: "#66d6e0",
-			light: "#bcf0f5",
-		},
-		background: {
-			default: "#fffbf3",
-			paper: "#f1e3b5",
-		},
-	},
-	typography: {
-		h1: {
-			fontSize: "3rem",
-			fontWeight: 500,
-			lineHeight: 1.2,
-		},
-		h2: {
-			fontSize: "2.5rem",
-			fontWeight: 500,
-		},
-		h3: {
-			fontSize: "2rem",
-			lineHeight: 1.15,
-			fontWeight: 400,
-		},
-		h4: {
-			fontSize: "1.7rem",
-			fontWeight: 400,
-			lineHeight: 1.15,
-		},
-		h5: {
-			fontWeight: 300,
-			lineHeight: 1.1,
-		},
-		h6: {
-			fontWeight: 300,
-			lineHeight: 1.1,
-		},
-		button: {
-			fontWeight: 600,
-		},
-	},
+    palette: {
+        primary: {
+            main: "#c9bfab",
+            contrastText: "#6d442b",
+        },
+        background: {
+            default: "#fffbf3",
+            paper: "#f1e3b5",
+        },
+    },
+    typography: {
+        h1: {
+            fontSize: "3rem",
+            fontWeight: 500,
+            lineHeight: 1.2,
+        },
+        h2: {
+            fontSize: "2.5rem",
+            fontWeight: 500,
+        },
+        h3: {
+            fontSize: "2rem",
+            lineHeight: 1.15,
+            fontWeight: 400,
+        },
+        h4: {
+            fontSize: "1.7rem",
+            fontWeight: 400,
+            lineHeight: 1.15,
+        },
+        h5: {
+            fontWeight: 300,
+            lineHeight: 1.1,
+        },
+        h6: {
+            fontWeight: 300,
+            lineHeight: 1.1,
+        },
+        button: {
+            fontWeight: 600,
+        },
+    },
+    components: {
+        MuiSwitch: {
+            styleOverrides: {
+                switchBase: {
+                    color: "#6d442b",
+                },
+                colorPrimary: {
+                    "&.Mui-checked": {
+                        color: "#6d442b",
+                    },
+                },
+                track: {
+                    opacity: 0.3,
+                    backgroundColor: "#6d442b",
+                    ".Mui-checked.Mui-checked + &": {
+                        opacity: 0.8,
+                        backgroundColor: "#6d442b",
+                    },
+                },
+            },
+        },
+    },
 });
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
-	</React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/shared/components/Settings/Settings.tsx
+++ src/shared/components/Settings/Settings.tsx
@@ -9,63 +9,69 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
 
 function Settings({ showAllCards, toggleAllCards }) {
-	const [isVisible, setIsVisible] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
 
-	const toggleSettings = () => setIsVisible(!isVisible);
+    const toggleSettings = () => setIsVisible(!isVisible);
 
-	return (
-		<div>
-			<IconButton
-				aria-label="settings"
-				sx={{ fontSize: "clamp(30px, 5vmax, 50px)" }}
-				onClick={toggleSettings}
-			>
-				<SettingsIcon fontSize="inherit" />
-			</IconButton>
-			{isVisible && (
-				<div className="overlay" onClick={toggleSettings}>
-					<div className="panel" onClick={(e) => e.stopPropagation()}>
-						<Grid container spacing={0}>
-							<Grid container size={12} alignItems={"center"}>
-								<Typography
-									variant="h5"
-									sx={{
-										flexGrow: 1,
-										color: "primary.main",
-										fontWeight: "bold",
-									}}
-								>
-									Settings
-								</Typography>
-								<IconButton
-									aria-label="settings"
-									size="medium"
-									onClick={toggleSettings}
-								>
-									<CloseIcon fontSize="inherit" />
-								</IconButton>
-							</Grid>
-							<Grid size={10}>
-								<FormGroup>
-									<FormControlLabel
-										control={
-											<Switch
-												checked={showAllCards}
-												onChange={toggleAllCards}
-											/>
-										}
-										label="Show All Cards"
-										labelPlacement="start"
-										sx={{ color: "primary.main" }}
-									/>
-								</FormGroup>
-							</Grid>
-						</Grid>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+    return (
+        <div>
+            <IconButton
+                aria-label="settings"
+                sx={{ fontSize: "clamp(30px, 5vmax, 50px)" }}
+                onClick={toggleSettings}
+            >
+                <SettingsIcon fontSize="inherit" />
+            </IconButton>
+            {isVisible && (
+                <div className="overlay" onClick={toggleSettings}>
+                    <Grid
+                        container
+                        spacing={0}
+                        onClick={(e) => e.stopPropagation()}
+                        sx={{
+                            padding: "15px",
+                            borderRadius: "10px",
+                            width: "300px",
+                            bgcolor: "primary.main",
+                        }}
+                    >
+                        <Grid container size={12} alignItems={"center"}>
+                            <Typography
+                                variant="h5"
+                                sx={{
+                                    flexGrow: 1,
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                Settings
+                            </Typography>
+                            <IconButton
+                                aria-label="settings"
+                                size="medium"
+                                onClick={toggleSettings}
+                            >
+                                <CloseIcon fontSize="inherit" />
+                            </IconButton>
+                        </Grid>
+                        <Grid size={10}>
+                            <FormGroup>
+                                <FormControlLabel
+                                    control={
+                                        <Switch
+                                            checked={showAllCards}
+                                            onChange={toggleAllCards}
+                                        />
+                                    }
+                                    label="Show All Cards"
+                                    labelPlacement="start"
+                                />
+                            </FormGroup>
+                        </Grid>
+                    </Grid>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default Settings;
