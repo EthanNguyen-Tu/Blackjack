1caa8c717aec3f3a8881c6396a8750ab22d6dfe6
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-03-15 09:44:16 -0400

    feat(Table): Generalized game table

--- src/shared/components/Table/Table.tsx
+++ src/shared/components/Table/Table.tsx
@@ -1,57 +1,8 @@
-import { Grid } from "@mui/system";
-import PlayingCard from "../PlayingCard/PlayingCard.tsx";
 import "./Table.css";
 import React from "react";
 
-function Table({ dealer_hand, player_hand }) {
-    return (
-        <div id="blackjack-table">
-            <Grid
-                container
-                direction="column"
-                justifyContent="space-between"
-                alignItems="center"
-                style={{ height: "85%" }}
-            >
-                <Grid
-                    container
-                    size={12}
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    {dealer_hand &&
-                        dealer_hand.getHand().map((card, idx) => (
-                            <PlayingCard
-                                card={card}
-                                sx={{
-                                    zIndex: idx,
-                                    marginLeft: "-75px",
-                                    marginTop: 25 * idx + "px",
-                                }}
-                            />
-                        ))}
-                </Grid>
-                <Grid
-                    container
-                    size={12}
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    {player_hand &&
-                        player_hand.getHand().map((card, idx) => (
-                            <PlayingCard
-                                card={card}
-                                sx={{
-                                    zIndex: idx,
-                                    marginLeft: "-75px",
-                                    marginTop: -25 * idx + "px",
-                                }}
-                            />
-                        ))}
-                </Grid>
-            </Grid>
-        </div>
-    );
+function Table({ game }) {
+    return <div id="game-table">{game}</div>;
 }
 
 export default Table;
