9d060cdfae6292c386295bd80c1a7d2a8d9f84c1
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-05-14 16:21:01 -0400

    build(web-vitals): updated and enabled web vitals to console

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,17 +1,15 @@
-import { ReportHandler } from "web-vitals";
+import { onCLS, onFCP, onINP, onLCP, onTTFB, type Metric } from "web-vitals";
+
+type ReportHandler = (metric: Metric) => void;
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import("web-vitals").then(
-			({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-				getCLS(onPerfEntry);
-				getFID(onPerfEntry);
-				getFCP(onPerfEntry);
-				getLCP(onPerfEntry);
-				getTTFB(onPerfEntry);
-			}
-		);
-	}
+    if (onPerfEntry && typeof onPerfEntry === "function") {
+        onCLS(onPerfEntry);
+        onFCP(onPerfEntry);
+        onINP(onPerfEntry);
+        onLCP(onPerfEntry);
+        onTTFB(onPerfEntry);
+    }
 };
 
 export default reportWebVitals;
