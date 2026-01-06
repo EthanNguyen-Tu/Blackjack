4f966a76253ada57be71f525d1751fdd9ef20a83
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2026-01-05 22:17:23 -0500

    fix(GameContext): provide default empty functions for setVictories and setTotalGames

--- src/app/shared/contexts/GameContext.ts
+++ src/app/shared/contexts/GameContext.ts
@@ -3,11 +3,13 @@ import { createContext } from "react";
 export interface GameContextContent {
     victories: number;
     totalGames: number;
-    setVictories?: React.Dispatch<React.SetStateAction<number>>;
-    setTotalGames?: React.Dispatch<React.SetStateAction<number>>;
+    setVictories: React.Dispatch<React.SetStateAction<number>>;
+    setTotalGames: React.Dispatch<React.SetStateAction<number>>;
 }
 
 export const GameContext = createContext<GameContextContent>({
     victories: 0,
     totalGames: 0,
+    setVictories: () => {},
+    setTotalGames: () => {},
 });
