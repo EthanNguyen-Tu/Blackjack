e9970f59abb5b8ef2b24c6a023cf3e1ee1318466
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2026-01-20 18:19:54 -0500

    feat(HandOfCardsDisplay): add vertical variant and click to rise to top

--- src/app/components/HandOfCards/HandOfCardsDisplay.tsx
+++ src/app/components/HandOfCards/HandOfCardsDisplay.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import PlayingCard from "../../shared/components/PlayingCard/PlayingCard";
+import styles from "./HandOfCardsDisplay.module.css";
+import { MAX_HAND_SIZE } from "@/app/shared/constants/playingcards";
 
 export enum HandOfCardsVariants {
     PLAYER = "Player",
@@ -10,56 +12,88 @@ interface HandOfCardsProps {
     variant: HandOfCardsVariants;
     hiddenCards?: string[];
     visibleCards?: string[];
+    vertical?: boolean;
+    key?: number;
 }
 
 function HandOfCardsDisplay(props: HandOfCardsProps) {
-    const { variant, hiddenCards, visibleCards } = props;
+    const { variant, hiddenCards, visibleCards, vertical, key } = props;
+    const [activeIdx, setActiveIdex] = useState<number | null>(null);
+    const cards: React.ReactNode[] = [];
 
     if (variant === HandOfCardsVariants.DEALER) {
-        const cards: React.ReactNode[] = [];
         let cardCount = 0;
         hiddenCards?.forEach((card) => {
+            const idx = cardCount;
             cards.push(
                 <PlayingCard
                     card={card}
                     flipped={true}
+                    onClick={() => setActiveIdex(idx)}
                     sx={{
-                        zIndex: cardCount,
-                        marginLeft: "-75px",
-                        marginTop: 25 * cardCount + "px",
+                        zIndex: activeIdx === idx ? MAX_HAND_SIZE : idx,
+                        marginTop: 25 * idx + "px",
+                        marginLeft: (vertical ?? idx === 0) ? "0" : "-40px",
+                        transform:
+                            activeIdx === idx ? "translateY(-20px)" : "none",
+                        cursor: "pointer",
                     }}
-                    key={`${variant}-Hidden-${cardCount}`}
-                />
+                    key={`${variant}-Hidden-${idx}`}
+                />,
             );
-            cardCount += 1;
+            cardCount++;
         });
         visibleCards?.forEach((card) => {
+            const idx = cardCount;
             cards.push(
                 <PlayingCard
                     card={card}
+                    onClick={() => setActiveIdex(idx)}
                     sx={{
-                        zIndex: cardCount,
-                        marginLeft: "-75px",
-                        marginTop: 25 * cardCount + "px",
+                        zIndex: activeIdx === idx ? MAX_HAND_SIZE : idx,
+                        marginLeft: (vertical ?? idx === 0) ? "0" : "-75px",
+                        transform:
+                            activeIdx === idx ? "translateY(-20px)" : "none",
+                        cursor: "pointer",
                     }}
-                    key={`${variant}-${card}-${cardCount}`}
-                />
+                    key={`${variant}-${card}-${idx}`}
+                />,
             );
-            cardCount += 1;
+            cardCount++;
         });
-        return cards;
+    } else {
+        visibleCards?.forEach((card, idx) =>
+            cards.push(
+                <PlayingCard
+                    card={card}
+                    onClick={() => setActiveIdex(idx)}
+                    sx={{
+                        zIndex: activeIdx === idx ? MAX_HAND_SIZE : idx,
+                        marginLeft: (vertical ?? idx === 0) ? "0" : "-75px",
+                        marginTop: vertical
+                            ? idx === 0
+                                ? "0"
+                                : "-125px"
+                            : "0",
+                        cursor: "pointer",
+                        transform:
+                            activeIdx === idx ? "translateY(-20px)" : "none",
+                        transition: "transform 0.2s ease",
+                    }}
+                    key={`${variant}-${card}-${idx}-${key ?? 0}`}
+                />,
+            ),
+        );
     }
-    return visibleCards?.map((card, idx) => (
-        <PlayingCard
-            card={card}
-            sx={{
-                zIndex: idx,
-                marginLeft: "-75px",
-                marginBottom: 25 * idx + "px",
-            }}
-            key={`${variant}-${card}-${idx}`}
-        />
-    ));
+
+    return (
+        <div
+            className={styles.container}
+            style={{ flexDirection: vertical ? "column" : "row" }}
+        >
+            {cards}
+        </div>
+    );
 }
 
 export default HandOfCardsDisplay;
